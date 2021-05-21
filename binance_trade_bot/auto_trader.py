41e5db16ad3fe825ff63d2830ded8cd224203f63
Jagz <jgonzabal@gmail.com>
2021-05-21 12:35:32 +0100

    Update wrong symbols order

--- binance_trade_bot/auto_trader.py
+++ binance_trade_bot/auto_trader.py
@@ -191,7 +191,7 @@ class AutoTrader:
 
                 current_coin = self.db.get_current_coin()
 
-                if current_coin == coin and coin.symbol != self.config.BRIDGE_SYMBOL:
+                if current_coin.symbol == coin.symbol and coin.symbol != self.config.BRIDGE_SYMBOL:
                     orders = self.manager.get_pair_orders(coin.symbol, self.config.BRIDGE_SYMBOL)
 
                     if len(orders) > 0:
@@ -221,7 +221,7 @@ class AutoTrader:
                                     f"Will be setting a stop loss order to buy at "
                                     + str(usd_value * (1 + self.config.MAXIMUM_LOSS / 100))
                                 )
-                                self.manager.set_buy_stop_loss_order(self.config.BRIDGE_SYMBOL, coin.symbol, usd_value)
+                                self.manager.set_buy_stop_loss_order(coin.symbol, self.config.BRIDGE_SYMBOL, usd_value)
                     # else:
                     #     self.manager.cancel_previous_orders(coin.symbol, self.config.BRIDGE_SYMBOL)
                     #     self.logger.info(
