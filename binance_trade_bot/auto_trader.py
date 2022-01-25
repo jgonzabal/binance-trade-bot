0c06d2ec31e8742073d7c8c1682ad699fd3d0c24
Javier Gonzalez-Zabaleta <jgonzabal@gmail.com>
2022-01-25 17:46:43 +0000

    Cast to float

--- binance_trade_bot/auto_trader.py
+++ binance_trade_bot/auto_trader.py
@@ -226,7 +226,7 @@ class AutoTrader:
             if coin.symbol == current_coin.symbol:
                 orders = self.manager.get_pair_orders(current_coin.symbol, self.config.BRIDGE_SYMBOL)
                 for order in orders:
-                    balance += order["origQty"]
+                    balance += float(order["origQty"])
 
             if balance == 0.0:
                 continue
